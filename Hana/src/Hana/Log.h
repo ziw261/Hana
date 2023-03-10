5bcdc739dee4b01ba9a2ec02104ee431d1382b5a
Azusa <akiriin0@gmail.com>
2023-03-09 23:10:35 -0800

    Logging
    https://www.youtube.com/watch?v=dZr-53LAlOw&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=6

---
+++ Hana/src/Hana/Log.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Hana
+{ 
+	class HANA_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define HN_CORE_TRACE(...)    ::Hana::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HN_CORE_INFO(...)     ::Hana::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HN_CORE_WARN(...)     ::Hana::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HN_CORE_ERROR(...)    ::Hana::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HN_CORE_FATAL(...)    ::Hana::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define HN_TRACE(...)         ::Hana::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HN_INFO(...)          ::Hana::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HN_WARN(...)          ::Hana::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HN_ERROR(...)         ::Hana::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HN_FATAL(...)         ::Hana::Log::GetClientLogger()->fatal(__VA_ARGS__)
