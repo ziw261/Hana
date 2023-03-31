c1c3232f3e9675c3b5b9ceadedf70649c56e4c83
Azusa <akiriin0@gmail.com>
2023-03-31 00:23:46 -0700

    Set up Layers

---
+++ Hana/src/Hana/Layer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Hana/Core.h"
+#include "Hana/Events/Event.h"
+
+namespace Hana
+{
+	class HANA_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+}
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,30 @@
 #include <Hana.h>
 
+class ExampleLayer : public Hana::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		HN_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Hana::Event& event) override
+	{
+		HN_TRACE("{0}", event);
+	}
+};
+
 class Sandbox : public Hana::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
