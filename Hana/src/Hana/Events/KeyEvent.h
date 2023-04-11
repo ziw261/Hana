87905ef290b35f756ac15acd538bac5af2f8a9bd
Azusa <akiriin0@gmail.com>
2023-04-10 22:19:46 -0700

    ImGui Event
    https://www.youtube.com/watch?v=yBP1gSbQPPM&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=16

--- Hana/src/Hana/Events/KeyEvent.h
+++ Hana/src/Hana/Events/KeyEvent.h
@@ -52,4 +52,20 @@ namespace Hana
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class HANA_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
