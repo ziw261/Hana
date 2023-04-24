e3bd3622547391a31967180520493abeab3fc054
Azusa <akiriin0@gmail.com>
2023-04-24 00:39:54 -0700

    Minor maintenance

--- Hana/src/Hana/Core.h
+++ Hana/src/Hana/Core.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #ifdef HN_PLATFORM_WINDOWS
+#if HN_DYNAMIC_LINK
 	#ifdef HN_BUILD_DLL
 		#define HANA_API __declspec(dllexport)
 	#else
 		#define HANA_API __declspec(dllimport)
 	#endif
+#else
+	#define HANA_API
+#endif
 #else
 	#error Hana only supports Windows!
 #endif
--- premake5.lua
+++ premake5.lua
@@ -25,9 +25,10 @@ include "Hana/vendor/imgui"
 
 project "Hana"
 	location "Hana"
-	kind "SharedLib"
+	kind "StaticLib"
 	language "C++"
-	staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -41,6 +42,11 @@ project "Hana"
 		"%{prj.name}/src/**.cpp",
 		"%{prj.name}/vendor/glm/glm/**.hpp",
 		"%{prj.name}/vendor/glm/glm/**.inl",
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
 	}
 
 	includedirs
@@ -62,7 +68,6 @@ project "Hana"
 	}
 
 	filter "system:windows"
-		cppdialect "C++17"
 		systemversion "latest"
 
 		defines
@@ -72,31 +77,27 @@ project "Hana"
 			"GLFW_INCLUDE_NONE",
 		}
 
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
-		}
-
 	filter "configurations:Debug"
 		defines "HN_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 
 	filter "configurations:Release"
 		defines "HN_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HN_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
-	staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -121,7 +122,6 @@ project "Sandbox"
 	}
 
 	filter "system:windows"
-		cppdialect "C++17"
 		systemversion "latest"
 
 		defines
@@ -132,14 +132,14 @@ project "Sandbox"
 	filter "configurations:Debug"
 		defines "HN_DEBUG"
 		runtime "Debug"
-		symbols "On"
+		symbols "on"
 
 	filter "configurations:Release"
 		defines "HN_RELEASE"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
 
 	filter "configurations:Dist"
 		defines "HN_DIST"
 		runtime "Release"
-		optimize "On"
+		optimize "on"
