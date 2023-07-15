206a99767b66cb4859551fbe46073698924dbd93
Azusa <akiriin0@gmail.com>
2023-07-15 00:11:10 -0700

    Refactor code from application to sandbox
    https://www.youtube.com/watch?v=4zj-0FQ7Xbg&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=36&ab_channel=TheCherno

--- Hana/src/Hana.h
+++ Hana/src/Hana.h
@@ -12,6 +12,17 @@
 
 #include "Hana/ImGui/ImGuiLayer.h"
 
+// ---Renderer------------------------------
+#include "Hana/Renderer/Renderer.h"
+#include "Hana/Renderer/RenderCommand.h"
+
+#include "Hana/Renderer/Buffer.h"
+#include "Hana/Renderer/Shader.h"
+#include "Hana/Renderer/VertexArray.h"
+
+#include "Hana/Renderer/OrthographicCamera.h"
+// -----------------------------------------
+
 // ---Entry Point---------------------------
 #include "Hana/EntryPoint.h"
-// ---Entry Point---------------------------
+// -----------------------------------------
