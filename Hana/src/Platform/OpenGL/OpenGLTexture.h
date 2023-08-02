014aea4becc5d5b93df1fc0c358df08411476a39
Azusa <32504776+ziw261@users.noreply.github.com>
2023-08-01 22:34:14 -0700

    Single shader (#46)
    We have two shaders, and when we draw quads, we keep switching shaders and this is neither necessary nor performance friendly. We can combine them into one shader, and create a pure white texture through code when we want color.

--- Hana/src/Platform/OpenGL/OpenGLTexture.h
+++ Hana/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,16 +2,21 @@
 
 #include "Hana/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace Hana
 {
 	class OpenGLTexture2D : public Texture2D
 	{
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
 		virtual uint32_t GetWidth() const override { return m_Width; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
+
+		virtual void SetData(void* data, uint32_t size) override;
 
 		virtual void Bind(uint32_t slot = 0) const override;
 
@@ -20,5 +25,6 @@ namespace Hana
 		uint32_t m_Width = 0;
 		uint32_t m_Height = 0;
 		uint32_t m_RendererID = 0;
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 }
